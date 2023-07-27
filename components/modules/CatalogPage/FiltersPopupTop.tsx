3380daf04351a687f70b2b96054a26bf88fe7702
Yuliya <iksulia1@gmail.com>
2023-07-27 12:51:45 +0300

    refactor

--- components/modules/CatalogPage/FiltersPopupTop.tsx
+++ components/modules/CatalogPage/FiltersPopupTop.tsx
@@ -4,14 +4,25 @@ import styles from '@/styles/catalog/index.module.scss'
 import { ICatalogFilterMobileProps, IFiltersPopupTop } from "@/types/catalog"
 import { title } from "process"
 
-const FiltersPopupTop = ({title, resetBtnText, resetFilters, resetFilterBtnDisabled, closePopup}: IFiltersPopupTop) => {
+const FiltersPopupTop = ({
+    title, 
+    resetBtnText, 
+    resetFilters, 
+    resetFilterBtnDisabled, 
+    closePopup
+}: IFiltersPopupTop) => {
     const mode = useStore($mode)
     const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
 
     return(
         <div className={`${styles.catalog__bottom__filters__top} ${darkModeClass}`}>
-            <button onClick={closePopup} className={styles.catalog__bottom__filters__title}>{title}</button>
-            <button onClick={resetFilters} disabled={resetFilterBtnDisabled} className={styles.catalog__bottom__filters__reset}>{resetBtnText}</button>
+            <button 
+            onClick={closePopup} 
+            className={`${styles.catalog__bottom__filters__title} ${darkModeClass}`}>{title}</button>
+            <button 
+            onClick={resetFilters} 
+            disabled={resetFilterBtnDisabled} 
+            className={styles.catalog__bottom__filters__reset}>{resetBtnText}</button>
         </div>
     )
 }
