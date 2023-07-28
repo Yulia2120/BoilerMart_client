6359f4aaf8c1a115a41a144a142609d25bc22465
Yuliya <iksulia1@gmail.com>
2023-07-28 12:49:37 +0300

    add toggleToCart

--- components/modules/CatalogPage/CatalogItem.tsx
+++ components/modules/CatalogPage/CatalogItem.tsx
@@ -10,14 +10,22 @@ import CartHoverCheckedSvg from "@/components/elements/CartHoverCheckedSvg/CartH
 import CartHoverSvg from "@/components/elements/CartHoverSvg/CartHoverSvg"
 import spinnerStyles from '@/styles/spinner/index.module.scss'
 import styles from '@/styles/catalog/index.module.scss'
+import { $user } from "@/context/user"
+import { toggleCartItem } from "@/utils/shopping-cart"
+import { removeFromCartFx } from "@/app/api/shopping-cart"
 
 
 const CatalogItem = ({item}: {item: IBoilerPart}) => {
     const mode = useStore($mode)
+    const user = useStore($user)
     const shoppingCart = useStore($shoppingCart)
     const isInCart = shoppingCart.some((cartItem) => cartItem.partId === item.id)
-    const [spinner, setSpinner] = useState(false)
+    const spinner = useStore(removeFromCartFx.pending)
     const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
+
+    const toggleToCart = () => toggleCartItem(user.username, item.id, isInCart)
+
+    
     return (
 
         <li className={`${styles.catalog__list__item} ${darkModeClass}`}>
@@ -26,11 +34,17 @@ const CatalogItem = ({item}: {item: IBoilerPart}) => {
                 <Link href={`/catalog/${item.id}`} passHref legacyBehavior>
                     <h3 className={styles.catalog__list__item__title}>{item.name}</h3>
                 </Link>
-                <span className={styles.catalog__list__item__code}>Артикул: {item.vendor_code}</span>
-                <span className={styles.catalog__list__item__price}>{formatPrice(item.price)} UAN</span>
+                <span className={styles.catalog__list__item__code}>
+                    Артикул: {item.vendor_code}
+                    </span>
+                <span className={styles.catalog__list__item__price}>
+                    {formatPrice(item.price)} UAN
+                    </span>
             </div>
-            <button className={`${styles.catalog__list__item__cart} ${isInCart ? styles.added : ''}`}
+            <button 
+            className={`${styles.catalog__list__item__cart} ${isInCart ? styles.added : ''}`}
             disabled={spinner}
+            onClick={toggleToCart}
             >
                 {spinner ? (<div className={spinnerStyles.spinner} style={{top: 6, left: 6}}/> ):
                  (<span>{isInCart ? <CartHoverCheckedSvg/> : <CartHoverSvg/>}</span>
