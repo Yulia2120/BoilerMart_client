641db0d720cbd9fe340b55d1c4b2e6e9b5546711
Yuliya <iksulia1@gmail.com>
2023-07-28 12:50:30 +0300

    add useEffect

--- components/modules/Header/CartPopup/CartPopupItem.tsx
+++ components/modules/Header/CartPopup/CartPopupItem.tsx
@@ -3,18 +3,30 @@ import { useStore } from "effector-react"
 import { IShoppingCartItem } from "@/types/shopping-cart"
 import Link from "next/link"
 import DeleteSvg from "@/components/elements/DeleteSvg/DeleteSvg"
-import { useState } from "react"
+import { useEffect, useState } from "react"
 import styles from '@/styles/cartPopup/index.module.scss'
 import spinnerStyles from '@/styles/spinner/index.module.scss'
 import { formatPrice } from "@/utils/common"
 import { removeItemFromCart } from "@/utils/shopping-cart"
+import { updateTotalPrice } from "@/utils/catalog"
 
 const CartPopupItem = ({item}: {item: IShoppingCartItem}) => {
     const mode = useStore($mode)
     const darkModeClass = mode === 'dark' ? `${styles.dark_mode}` : ''
     const spinnerDarkModeClass = mode === 'dark' ? `${spinnerStyles.dark_mode}` : ''
     const [spinner, setSpinner] = useState(false)
+    const [price, setPrice] = useState(item.price)
 
+    useEffect(() => {
+        setPrice(price * item.count)
+    }, [])
+
+    useEffect(() => {
+        updateTotalPrice(price, item.partId)
+    }, [price])
+    
+    const increasePrice = () => setPrice(price + item.price)
+    const decreasePrice = () => setPrice(price - item.price)
     const deleteCartItem = () => removeItemFromCart(item.partId)
 
     return (
