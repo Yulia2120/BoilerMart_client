20b65cc4786a311f2aad0e8a6d978a99c9bd3084
Yuliya <iksulia1@gmail.com>
2023-08-04 08:18:53 +0300

    add OrderItem.tsx

---
+++ app/api/payment.ts
@@ -0,0 +1,17 @@
+import { createEffect } from "effector"
+import api from '../axiosClient'
+import { ICheckPayFx, IMakePayFx } from "@/types/order"
+
+export const makePaymentFx = createEffect(
+    async({url, amount}: IMakePayFx) =>{
+    const {data} = await api.post(url, {amount})
+
+    return data
+})
+
+export const checkPaymentFx = createEffect(
+    async({url, paymentId}: ICheckPayFx) =>{
+    const {data} = await api.post(url, {paymentId})
+
+    return data
+})
